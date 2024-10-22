6845e963f6e8add1635958a54f87ea5243574a07
Marcin Pałys <marcin.palys@microsoft.wsei.edu.pl>
2024-10-22 21:46:28 +0200

    Dodanie projektu

---
+++ ASP_Contact/Controllers/ContactController.cs
@@ -0,0 +1,84 @@
+﻿using ASP_Contact.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace ASP_Contact.Controllers
+{
+    public class ContactController : Controller
+    {
+        public static Dictionary<int, Contact> _contacts = new Dictionary<int, Contact>();
+        public IActionResult Index()
+        {
+            return View(_contacts);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                int id = _contacts.Keys.Count != 0 ? _contacts.Keys.Max() : 0;
+                contact.id = id + 1;
+                _contacts.Add(contact.id, contact);
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        [HttpGet]
+        public IActionResult Edit(int id) 
+        {
+            if (_contacts.Keys.Contains(id))
+            {
+                return View(_contacts[id]);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        [HttpPost]
+        public IActionResult Edit(Contact contact) 
+        {
+            if (ModelState.IsValid) 
+            {
+                _contacts[contact.id] = contact;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public IActionResult Details(int id) 
+        { 
+            if(_contacts.Keys.Contains(id))
+            {
+                return View(_contacts[id]);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        public IActionResult Delete(int id)
+        {
+            if(_contacts.Keys.Contains(id))
+            {
+                _contacts.Remove(id);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ ASP_Contact/Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASP_Contact.Models
+{
+    public class Contact
+    {
+        [HiddenInput]
+        public int id { get; set; }
+        [Required(ErrorMessage = "Proszę podać imię")]
+        public string name { get; set; }
+        [Required(ErrorMessage = "Proszę podać nazwisko")]
+        public string surname { get; set; }
+        [Required(ErrorMessage = "Proszę podać numer telefonu")]
+        public string phone { get; set; }
+        [Required(ErrorMessage = "Proszę podać email")]
+        public string email { get; set; }
+        [Required(ErrorMessage = "Proszę podać datę urodzenia")]
+        public DateTime dateOfBirth { get; set; }
+    }
+}
